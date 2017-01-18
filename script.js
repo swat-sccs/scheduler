666606795c3cc908497f1a4fee5e146a6b147da0
Jonah <jonahmail1@gmail.com>
2017-01-17 21:58:10 -0500

    Change where append

--- script.js
+++ script.js
@@ -477,7 +477,7 @@ function handleAuthCheckButton(authResult){
                  batch.execute(function(resp){
                          console.log("deleted")
                      console.log(resp)
-                             $("#authorize").append('<b>Success! You now have a new calendar called "SCCS Class Schedule" in your Google Calendar (might need to refresh)</b><br>')
+                             $("#exportReady").append('<b>Success! You now have a new calendar called "SCCS Class Schedule" in your Google Calendar (might need to refresh)</b><br>')
                  })
              })
          }
