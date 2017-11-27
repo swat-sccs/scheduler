7bff04776eda115b0d1cd06d78976f111cf54025
Jonah Langlieb <jonahmail1@gmail.com>
2017-11-27 00:08:11 -0500

    Move gAnalytics to after main

--- js/main.js
+++ js/main.js
@@ -149,6 +149,11 @@ $( document ).ready(function() {
         /* })*/
 
     });
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-90553706-1'); 
+
     $('.longListId').change(longListCallback);
     notFromHash = true;
     if(window.location.hash!==""){
