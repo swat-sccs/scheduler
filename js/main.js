f41c9cdbdfdb794fc094908f34b5e7193514a35e
David Bauman <dbauman1@swarthmore.edu>
2021-04-22 21:27:51 -0400

    Collocates all the per-semester changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 // TODO REDO EVERY SEMESTER SO GOOGLE CALENDAR ONLY CLEARS OUT MOST RECENT
-
-// UPDATE NEW SEMESTER HERE
+/* UPDATE NEW SEMESTER section */
 // helps gCal disambiguate each semester's events so can delete only the newest semeter if change schedule
 const term = 'fall21'
 const termSubtitle = 'Fall 2021' // used as text for #semester-subtitle in index.html
+
+// Go to https://www.swarthmore.edu/registrar/five-year-calendar and fill in
+// month number are 0 indexed! so -1
+// test in javascript console by doing `new Date(... below ...)`
+const startSemesterTime = Date.UTC(2021, 7, 30, 0, 0, 0)
+
+// Just change the part before "T" as normal, not 0 indexed
+// Inclusive but needs to be at T235959Z so gets whole day when ends (can also be the next day (exclusive)T000000Z but that isn't ideal if have whole-day events)
+// TODO known issue with timezones (Z is UTC) for late running events on the last day
+const endSemesterISO = '20211218T235959Z'
+
+/* end UPDATE NEW SEMESTER section */
 
 let selectedClasses = []
 let classSchedObj
@@ -18,17 +29,7 @@ let fullCal
 // Needs to be global so hacker list can tell when classes do not fit
 // TODO too long?
 const daysTimesRanges = [[], [], [], [], [], [], [], []]
-// UPDATE NEW SEMESTER HERE
-// Go to https://www.swarthmore.edu/registrar/five-year-calendar and fill in
-// month number are 0 indexed! so -1
-// test in javascript console by doing `new Date(... below ...)`
-const startSemesterTime = Date.UTC(2021, 7, 30, 0, 0, 0)
 
-// UPDATE NEW SEMESTER HERE
-// Just change the part before "T" as normal, not 0 indexed
-// Inclusive but needs to be at T235959Z so gets whole day when ends (can also be the next day (exclusive)T000000Z but that isn't ideal if have whole-day events)
-// TODO known issue with timezones (Z is UTC) for late running events on the last day
-const endSemesterISO = '20211218T235959Z'
 const startSemDate = new Date(startSemesterTime)
 /* TODO add holidays manually
    var exceptionDays = "";
