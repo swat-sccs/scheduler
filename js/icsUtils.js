cb2e2e1a7beb5b9bb462ebfd294d88b1e59fa926
Thomas Makin <halorocker89@gmail.com>
2022-01-23 14:34:57 -0500

    Formatting changes and import structure

--- js/icsUtils.js
+++ js/icsUtils.js
@@ -1,22 +1,20 @@
 const { writeFileSync } = require('fs')
 const ics = require('ics')
 
-function buildEvent(name, room, start, duration, days, classEnd) {
-    ics.createEvent({
-        title: name,
-        description: room,
-        busyStatus: 'FREE',
-        start: start, // Format: [yyyy, mm, dd, hh, mm]
-        recurrenceRule: 'FREQ=WEEKLY;BYDAY=' + days // Format: SU,MO,TU,WE,TH,FR,SA
-                        + ';INTERVAL=1;UNTIL=' + classEnd // Format: yyyymmdd
-                        + 'T050000Z',
-                        // https://www.textmagic.com/free-tools/rrule-generator
-        duration: { minutes: 50 }
-      }, (error, value) => {
-        if (error) {
-          console.log(error)
-        }
-      
-        writeFileSync(`${__dirname}/event.ics`, value)
-      })
+export function buildEvent(name, room, start, duration, days, classEnd) {
+  const event = {
+    title: name,
+    description: room,
+    busyStatus: 'FREE',
+    start: start, // Format: [yyyy, mm, dd, hh, mm]
+    recurrenceRule: 'FREQ=WEEKLY;BYDAY=' + days // Format: SU,MO,TU,WE,TH,FR,SA
+                    + ';INTERVAL=1;UNTIL=' + classEnd // Format: yyyymmdd
+                    + 'T050000Z',
+                    // https://www.textmagic.com/free-tools/rrule-generator
+    duration: { minutes: 50 }
+  }
+  return event;
+}
+
+function buildFile(events) {
 }
