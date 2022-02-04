5399de938f0cc823aac17f4ae063b05cf369b23b
Thomas Makin <halorocker89@gmail.com>
2022-02-04 05:19:15 +0000

    Very close to done--time parsing is buged

--- js/icsUtils.js
+++ js/icsUtils.js
@@ -5,11 +5,12 @@ export function buildEvent(name, start, end, days, classEnd) {
   const event = {
     title: name,
     busyStatus: 'BUSY',
-    start: start, // Format: [yyyy, mm, dd, hh, mm]
-    end: end,
+    start: [parseInt(start[0]), parseInt(start[1]), parseInt(start[2]), 
+            parseInt(start[3]), parseInt(start[4])], // Format: [yyyy, mm, dd, hh, mm]
+    end: [parseInt(end[0]), parseInt(end[1]), parseInt(end[2]), 
+          parseInt(end[3]), parseInt(end[4])],
     recurrenceRule: 'FREQ=WEEKLY;BYDAY=' + days // Format: SU,MO,TU,WE,TH,FR,SA
-                    + ';INTERVAL=1;UNTIL=' + classEnd // Format: yyyymmdd
-                    + 'T050000Z',
+                    + ';INTERVAL=1;UNTIL=' + classEnd // Format: yyyymmddT$timeutc
                     // https://www.textmagic.com/free-tools/rrule-generator
   }
   return event;
