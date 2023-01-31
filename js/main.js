ec3c19810409828a7383355da98ae6e7a57e6f04
tonykcao <thienkhangcao@gmail.com>
2023-01-31 09:56:55 -0500

    style: adjust button styling and add anim

--- js/main.js
+++ js/main.js
@@ -246,15 +246,15 @@ function updateHashCookie() {
 }
 
 function registerSW() {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-          navigator.serviceWorker.register('/service-worker.js').then(registration => {
-            console.log('SW registered: ', registration);
-          }).catch(registrationError => {
-            console.log('SW registration failed: ', registrationError);
-          });
-        });
-      }
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('SW registered: ', registration);
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+    });
+  }
 }
 
 function setupEventListeners() {
