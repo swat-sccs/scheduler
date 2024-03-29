8c42f2f9bfc20f3a33d3d45e0ae3684dca89d74f
Thomas Makin <halorocker89@gmail.com>
2024-03-29 01:34:49 -0400

    update for fall 24

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 // TODO REDO EVERY SEMESTER SO GOOGLE CALENDAR ONLY CLEARS OUT MOST RECENT
 // helps gCal disambiguate each semester's events so can delete only the newest semeter if change schedule
-const term = 'spring24'
-const termSubtitle = 'Spring 2024' // used as text for #semester-subtitle in index.html
+const term = 'fall24'
+const termSubtitle = 'Fall 2024' // used as text for #semester-subtitle in index.html
 
 // Tricoschedule may not be updated when the schedule first comes out
 // If this is the case, use the xls scraped
@@ -9,9 +9,9 @@ const scheduleJSON = 'trico_scraped.json'
 // const scheduleJSON = 'js/xls_scraped.json'
 
 // Go to https://www.swarthmore.edu/registrar/five-year-calendar and fill in
-const startSemester = ['2024', '01', '22'];
-const endHalfSemester = ['2024', '03', '18'];
-const endSemester = ['2024', '05', '06'];
+const startSemester = ['2024', '09', '01'];
+const endHalfSemester = ['2024', '10', '12'];
+const endSemester = ['2024', '12', '14'];
 
 // RRULE generator says use 50000Z so...I'm not Google go look it up
 const endSemesterISO = endSemester[0] + endSemester[1] + endSemester[2]
